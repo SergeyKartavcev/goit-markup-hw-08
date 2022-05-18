e40170ab5e7e06d68201b41de76e010ed6982751
SergeyKartavcev <kartavcev1987serg@gmail.com>
2022-05-18 09:52:30 +0300

    add

--- js/mobil-menu.js
+++ js/mobil-menu.js
@@ -1,14 +1,14 @@
-(() => {
-const menuBtnRef = document.querySelector("[data-menu-button]");
+// (() => {
+// const menuBtnRef = document.querySelector("[data-menu-button]");
 
-menuBtnRef.addEventListener("click",() =>{
-   const expanded =
+// menuBtnRef.addEventListener("click",() =>{
+//    const expanded =
 
 
- menuBtnRef.getAttribute("aria-expanded")==="true"||false;
-menuBtnRef.classList,toggle("is-open");
+//  menuBtnRef.getAttribute("aria-expanded")==="true"||false;
+// menuBtnRef.classList,toggle("is-open");
 
- menuBtnRef.setAttribute("aria-enpanded",!expanded);
- mobileMenuRef.classList,toggle("is-open");
- });
-})();
+//  menuBtnRef.setAttribute("aria-enpanded",!expanded);
+//  mobileMenuRef.classList,toggle("is-open");
+//  });
+// })();
